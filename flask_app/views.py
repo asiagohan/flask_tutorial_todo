5efee6825270894b1c897be5ea57e7c85588a16f
asiagohan <akan1235@gmail.com>
2015-08-10 12:02:43 +0900

    model added
    database is available

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,7 +1,9 @@
+import datetime
 from flask import render_template, flash, redirect, url_for, request
 
-from flask_app import app
+from flask_app import app, db
 from .forms import TodoForm
+from .models import Todo
 
 @app.route('/')
 def hello_world():
@@ -13,10 +15,15 @@ def register():
     if request.method == 'POST' and form.validate():
         title = form.title.data
         detail = form.detail.data
-        flash('title:{title}, detail:{detail}'.format(title=title, detail=detail))
+        timestamp = datetime.datetime.utcnow()
+        todo = Todo(title=title, detail=detail, timestamp=timestamp)
+        db.session.add(todo)
+        db.session.commit()
         return redirect(url_for('register'))
 
+    todo_list = Todo.query.order_by(Todo.timestamp.desc())
     return render_template('register.html',
-                           form=form)
+                           form=form,
+                           todo_list=todo_list)
 
 
