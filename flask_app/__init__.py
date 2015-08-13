14270df9998469bd9057b953b905a77ffce93cd2
asiagohan <akan1235@gmail.com>
2015-08-13 22:32:57 +0900

    custom filter added
    datetimeformat and newlinetobr filter added

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+from .datetimeformat import datetimeformat
+from .newlinetobr import newlinetobr
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+app.jinja_env.filters['datetimeformat'] = datetimeformat
+app.jinja_env.filters['newlinetobr'] = newlinetobr
 
 from flask_app import views, models
